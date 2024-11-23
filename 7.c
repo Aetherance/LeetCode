88b3ece2c0c4c7afdbb4166d21218d17de3797f6
User <2085163736@qq.com>
2024-11-24 00:02:16 +0800

    UPDATE FILES

---
+++ 7.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+long long reverse(int x) {
+    long flag = 1;
+    long long turned = 0;
+    if(x<0)
+    {
+        flag = -1;
+        x = -x;
+
+    }
+    
+    while(x)
+    {
+        if(turned*10>2e31)
+            return 0;
+        turned *= (long long)10;
+        turned += (long long)x% 10;
+        x/=10;
+    }
+    if((turned)<2e31-1)
+        return 0;
+
+    return turned*flag;
+}
+int main()
+{
+    printf("%lld",reverse(1534236469));
+    printf("%lld",2e31);
+
+
+
+
+
+    return 0;
+}
