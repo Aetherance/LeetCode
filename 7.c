6472e8c15cea8f75265608f19fcf57937810422a
User <2085163736@qq.com>
2024-11-24 17:44:43 +0800

    update 7.c

--- 7.c
+++ 7.c
@@ -1,24 +1,23 @@
 #include<stdio.h>
-long long reverse(int x) {
-    long flag = 1;
+int reverse(long long x) {
+    int flag = 1;
     long long turned = 0;
+    
     if(x<0)
     {
         flag = -1;
         x = -x;
-
     }
     
     while(x)
     {
-        if(turned*10>2e31)
-            return 0;
-        turned *= (long long)10;
-        turned += (long long)x% 10;
+        turned *= 10;
+        turned += x% 10;
         x/=10;
     }
-    if((turned)>2e31-1)
+    if((turned)>2147483647-1||turned*(flag)<-2147483647)
         return 0;
+
     return turned*flag;
 }
 int main()
