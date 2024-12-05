e4b7dbb531737c51332c575ccfee9214fa089f8f
User <2085163736@qq.com>
2024-12-05 10:17:34 +0800

    update files

---
+++ 贪心算法/3.cpp
@@ -0,0 +1,20 @@
+// 1833. 雪糕的最大数量
+
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        int hash[100005] = {0};
+        for (auto& c : costs) {
+            hash[c]++;
+        }
+        int ret = 0;
+        for (int i = 1; i < 100005; i++) {
+            if (coins < i)
+                break;
+            int tmp = min(coins / i, hash[i]);
+            coins -= tmp * i;
+            ret += tmp;
+        }
+        return ret;
+    }
+};
