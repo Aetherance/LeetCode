946f69bd7e59f2b1f661122ade466645c217e963
User <2085163736@qq.com>
2024-12-05 10:22:38 +0800

    update files

--- 贪心算法/3.cpp
+++ 贪心算法/3.cpp
@@ -8,18 +8,17 @@ using namespace std;
 class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
-        int hash[100005] = {0};
-        for (auto& c : costs) {
-            hash[c]++;
-        }
-        int ret = 0;
-        for (int i = 1; i < 100005; i++) {
-            if (coins < i)
+        sort(costs.begin(),costs.end());
+        int count = 0;
+        for(int i = 0;i<costs.size();i++)
+        {
+            coins -= costs[i];
+            count ++;
+            if(coins <= 0)
                 break;
-            int tmp = min(coins / i, hash[i]);
-            coins -= tmp * i;
-            ret += tmp;
         }
-        return ret;
+        if(coins<0)
+            count --;
+        return count;
     }
 };
